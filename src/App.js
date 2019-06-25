4dfac0b0b9c9b464d3d8f79e0546d2c941ae5589
DESKTOP-KEONA0H\LBP-PC <pglbuddhika@gmail.com>
2019-06-25 06:44:05 +0530

    initial
    initial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-class App extends Component {
+export default class App extends React.Component {
+  state = {
+    email: '',
+    password: ''
+  }
+
+  handleChange = event => {
+    this.setState({ email: event.target.value });
+    this.setState({ password: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    axios.post(`http://mygym.test:8888/controller/loginController.php`, { user })
+      .then(res => {
+        console.log(res);
+        console.log(res.data);
+      })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Email:
+            <input type="text" name="email" onChange={this.handleChange} />
+          </label>
+          <label>
+            Password:
+            <input type="password" name="password" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
       </div>
-    );
+    )
   }
 }
 
-export default App;
